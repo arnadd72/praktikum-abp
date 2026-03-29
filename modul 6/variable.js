21d9c59ba8d94c8cd5480993ada4f6b6ccce9a59
arnadd72 <149177348+arnadd72@users.noreply.github.com>
2026-03-29 16:54:32 +0700

    Add files via upload

---
+++ modul 6/variable.js
@@ -0,0 +1,11 @@
+// Deklarasi variabel menggunakan kata kunci var
+var nama = "Arnanda setya nosa putra"; // Tipe data String 
+var nim = "2311102180";       // Tipe data String 
+
+// Menampilkan isi variabel ke terminal
+console.log("Halo, nama saya " + nama);
+console.log("NIM saya adalah " + nim);
+
+// Variabel juga bisa menampung angka (Number)
+var umur = 20;
+console.log("Umur: " + umur + " tahun");
